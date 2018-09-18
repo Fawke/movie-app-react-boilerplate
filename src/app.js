2db23ece17bf1b5f8207c9bb92b5727afb392ff3
Neelanjan.Sen <neelanjan.sen@stackroute.in>
2018-09-18 15:26:44 +0530

    added a button component to check if material-ui is configured correctly

--- src/app.js
+++ src/app.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+import Button from '@material-ui/core/Button'
+
+const Home = () => (
+  <div>
+    <p>React basic boilerplate with Material-UI configured</p>
+    <Button variant="contained" color="primary"> 
+      Home
+    </Button> 
+  </div>
+);
+
 ReactDOM.render(
-  <p>This is my boilerplate</p>, 
+  <Home />, 
   document.getElementById('app')
 );
